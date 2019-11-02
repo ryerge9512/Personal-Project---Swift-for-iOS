cc372de315a1921a33ba08e299e8af1927b1f2c4
Ryan Yerge <ryerge9512@eagle.fgcu.edu>
2019-11-02 00:13:27 -0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Ryan Yerge on 11/2/19.
+//  Copyright © 2019 COP 3003 - Ryan Yerge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
